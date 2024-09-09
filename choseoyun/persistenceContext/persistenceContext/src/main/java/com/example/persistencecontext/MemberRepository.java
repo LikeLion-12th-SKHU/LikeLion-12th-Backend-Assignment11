b543e3fb3c03ebabfbd1035d91aadfc5ddf76e23
seoyun <seoyun-cho@naver.com>
2024-09-09 23:16:21 +0900

    10주차 과제

---
+++ choseoyun/persistenceContext/persistenceContext/src/main/java/com/example/persistencecontext/MemberRepository.java
@@ -0,0 +1,15 @@
+package com.example.persistencecontext;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MemberRepository {
+    Member save(Member member);
+    List<Member> getAllMembers();
+    Member getMemberById(Long id);
+    Member updateMember(Long id, String name);
+    void deleteMember(Long id);
+    void checkEntity(Long id);
+}
